e5098e4526324269c6356834c9716d615a8e6551
sopark <sopark@daou.co.kr>
2018-08-25 14:24:24 +0900

    init

---
+++ server/src/main/java/com/web/tempalte/TempalteApplication.java
@@ -0,0 +1,12 @@
+package com.web.tempalte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempalteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempalteApplication.class, args);
+    }
+}
