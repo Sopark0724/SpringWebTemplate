33f902455195f1e6a24b2ac49a4953bfffde2d43
lhs1553 <lhs1553@gmail.com>
2018-09-03 20:36:24 +0900

    cors mapping 추가

--- server/src/main/java/com/web/tempalte/security/SpringSecurityConfig.java
+++ server/src/main/java/com/web/tempalte/security/SpringSecurityConfig.java
@@ -8,13 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  */
 @Configuration
 @EnableWebSecurity
 @Slf4j
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,11 +32,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+    }
 }
