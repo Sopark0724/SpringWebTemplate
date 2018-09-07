502f85880774b18af0aef0a327a67b675ecdbf15
sopark <sopark@daou.co.kr>
2018-09-07 23:29:34 +0900

    feature/조직도_트리구조_개발 - interface 분리

--- server/src/main/java/com/web/template/org/api/OrgJPAController.java
+++ server/src/main/java/com/web/template/org/api/OrgJPAController.java
@@ -1,10 +1,11 @@
 package com.web.template.org.api;
 
-import com.web.template.org.application.OrgServiceJPAImpl;
+import com.web.template.org.application.OrgService;
 import com.web.template.org.application.data.DepartmentAddCommand;
 import com.web.template.org.application.data.DepartmentPresentation;
 import com.web.template.org.application.data.OrgPresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrgJPAController {
 
     @Autowired
-    private OrgServiceJPAImpl orgServiceJPA;
+    @Qualifier(value = "orgSercviceJPAImpl")
+    private OrgService orgService;
 
     @PostMapping("/department")
     public DepartmentPresentation create(DepartmentAddCommand addCommand) {
-        DepartmentPresentation departmentPresentation = orgServiceJPA.create(addCommand);
+        DepartmentPresentation departmentPresentation = orgService.create(addCommand);
         return departmentPresentation;
     }
 
 
     @GetMapping("/org")
     public OrgPresentation getOrg(){
-        return orgServiceJPA.getTree();
+        return orgService.getTree();
     }
 }
