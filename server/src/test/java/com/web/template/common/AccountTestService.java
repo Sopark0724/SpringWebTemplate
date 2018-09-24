1f198564fe19a639f64e9a3fdf259d6be589f5f4
hsim <lhs1553@gmail.com>
2018-09-24 17:41:11 +0900

    import 에러 수정

--- server/src/test/java/com/web/template/common/AccountTestService.java
+++ server/src/test/java/com/web/template/common/AccountTestService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Service;
-import sun.applet.resources.MsgAppletViewer;
 
 import java.util.HashMap;
 import java.util.Map;
